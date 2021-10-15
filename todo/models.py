89de8bbdae8197fa844262d56ec951648262103a
johnvenkiah <contact@johnvenkiah.com>
2021-10-15 16:34:15 +0000

    add items to views.py

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        # Redifine the name
+        return self.name
