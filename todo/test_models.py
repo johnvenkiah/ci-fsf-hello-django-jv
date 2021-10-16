3116e40719e0872d2a9407ea330dc8b62e80d1e2
johnvenkiah <contact@johnvenkiah.com>
2021-10-16 18:52:09 +0000

    create tests to achieve 100% coverage in tests

--- todo/test_models.py
+++ todo/test_models.py
@@ -6,3 +6,8 @@ class TestModels(TestCase):
     def test_done_defaults_to_false(self):
         item = Item.objects.create(name='Test To Do Item')
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test To Do Item')
+        # confirm that the name is returned when rendering the item as string
+        self.assertEqual(str(item), 'Test To Do Item')
--- todo/test_views.py
+++ todo/test_views.py
@@ -61,3 +61,15 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+
+    def test_can_edit_item(self):
+        """
+        Testing the http response with django's built in client, test create an
+        item and deleting it. Also that the correct template is used.
+        """
+        item = Item.objects.create(name='Test To Do Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
