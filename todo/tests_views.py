0c4502038d8ab8fe7c6b46b5a7e3e249dad9e726
johnvenkiah <contact@johnvenkiah.com>
2021-10-16 17:49:28 +0000

    create test for forms, create first test for views

--- todo/tests_views.py
+++ todo/tests_views.py
@@ -1,8 +1,17 @@
 from django.test import TestCase
 
 
-# Create your tests here.
-class TestDjango(TestCase):
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+class TestViews(TestCase):
+    def test_get_todo_list(self):
+        """Testing the http response with django's built in client"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
 
+    # def test_get_add_item_page(self):
+
+    # def test_get_edit_item_page(self):
+
+    # def test_can_add_item(self):
+
+    # def test_can_delete_item(self):
